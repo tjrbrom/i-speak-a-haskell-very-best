9364f06907657ba71857cfb3b24ba0b5ed44382f
Nik Them <tjrbrom@gmail.com>
2020-01-26 22:39:07 +0200

    Update baby.hs

--- baby.hs
+++ baby.hs
@@ -10,3 +10,10 @@ take' :: (Integral i, Ord a) => i -> [a] -> [a]
 take' n xs | n <= 0 = []
 take' n xs = case xs of [] -> []
                         (x:xs) -> x : take' (n-1) xs
+
+zip' :: [a] -> [b] -> [(a, b)]
+
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
