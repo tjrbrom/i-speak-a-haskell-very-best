4a9bb9efcc944cec82ae49337447999c412dcce7
Nik Them <tjrbrom@gmail.com>
2020-01-26 20:10:04 +0200

    Update baby.hs
    hk

--- baby.hs
+++ baby.hs
@@ -5,3 +5,8 @@ repl' x y = if(y <= 1)
 	    then [x] 
 	    else [x] ++ (repl' x (y-1))
 
+take' :: (Integral i, Ord a) => i -> [a] -> [a]
+
+take' n xs | n <= 0 = []
+take' n xs = case xs of [] -> []
+                        (x:xs) -> x : take' (n-1) xs
